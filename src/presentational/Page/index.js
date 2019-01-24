3b3945631a99343aa73a77ab96e8c85d01df6182
Dima Devgru <git@devg.ru>
2019-01-24 09:44:13 +0300

    Separate Page css

--- src/presentational/Page/index.js
+++ src/presentational/Page/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-export default ({ children, inverse }) => (inverse && (
-  <div>
-    <div className="Tutorial-page Tutorial-page_inverse_open" />
-    <div className="Tutorial-page Tutorial-page_inverse">{children}</div>
-    <div className="Tutorial-page Tutorial-page_inverse_close" />
-  </div>
-)) || (
-  <div className="Tutorial-page">{children}</div>
-);
+import './index.css';
+
+export default ({ children, inverse }) =>
+  (inverse && (
+    <div>
+      <div className="Tutorial-page Tutorial-page_inverse_open" />
+      <div className="Tutorial-page Tutorial-page_inverse">{children}</div>
+      <div className="Tutorial-page Tutorial-page_inverse_close" />
+    </div>
+  )) || <div className="Tutorial-page">{children}</div>;
