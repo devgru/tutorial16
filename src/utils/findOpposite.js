274feaf823ca957c7de59cb2efa165bcca7067b4
Dmitriy Semyushkin <dsemyushkin@tango.me>
2022-12-11 20:56:00 +0200

    Fix findOpposite

--- src/utils/findOpposite.js
+++ src/utils/findOpposite.js
@@ -2,59 +2,59 @@ import { scaleLinear } from 'd3-scale';
 import delta from './delta';
 import interpolateLabObject from './interpolateLabObject';
 
-const FIND_EPSILON = 1 / 1024;
-const EPSILON = 1 / 16;
+const DIFF_EPSILON = 1 / 1024;
+const SCALE_EPSILON = 1 / 1024;
 const MIN_DELTA = 1;
 
 const bisectFind = (scale, searchValue, l, r) => {
   const diff = r - l;
-  if (diff <= FIND_EPSILON) {
-    console.log('EPSILON REACHED');
+  if (diff <= DIFF_EPSILON) {
     return l;
   }
 
   const mid = (l + r) / 2;
-  const lColor = scale(l);
-  const rColor = scale(r);
-  const lDelta = delta(searchValue, lColor);
-  const rDelta = delta(searchValue, rColor);
+  const lDelta = delta(searchValue, scale(l));
+  const rDelta = delta(searchValue, scale(r));
   if (lDelta < rDelta) {
     if (lDelta < MIN_DELTA) {
       return l;
     }
     return bisectFind(scale, searchValue, l, mid);
+  } else {
+    if (rDelta < MIN_DELTA) {
+      return r;
+    }
+    return bisectFind(scale, searchValue, mid, r);
   }
-  if (rDelta < MIN_DELTA) {
-    return r;
-  }
-  return bisectFind(scale, searchValue, mid, r);
 };
 
-const bisect = (scale, init, step) => {
+const bisectGamutEdge = (scale, init, step) => {
   const color = scale(init + step);
   if (color.displayable()) {
-    if (step <= EPSILON) {
+    if (step <= SCALE_EPSILON) {
       return color.hex();
     }
-    return bisect(scale, init + step, step / 2);
+    return bisectGamutEdge(scale, init + step, step * 2);
   } else {
-    return bisect(scale, init, step / 2);
+    return bisectGamutEdge(scale, init, step / 2);
   }
 };
 
 const findOpposite = (currentColor, midColor = '#808080') => {
   const scale = scaleLinear()
     .range([currentColor, midColor])
+    .domain([-1, 1])
     .interpolate(interpolateLabObject);
 
-  const currentColorEdge = bisect(scale, 1, +16);
-  const oppositeColorEdge = bisect(scale, 1, -16);
+  const currentColorEdge = bisectGamutEdge(scale, 0, -1);
+  const oppositeColorEdge = bisectGamutEdge(scale, 0, +1);
 
   const edgeScale = scaleLinear()
-    .range([currentColorEdge, midColor, oppositeColorEdge])
+    .range([currentColorEdge, oppositeColorEdge])
+    .domain([-1, 1])
     .interpolate(interpolateLabObject);
-  const colorPosition = bisectFind(edgeScale, currentColor, 1, 2);
-  return edgeScale(2 - colorPosition);
+  const colorPosition = bisectFind(edgeScale, currentColor, -1, 1);
+  return edgeScale(-colorPosition);
 };
 
 export default findOpposite;
